c40d321168f0aeb68cdd8e3403608b471705210b
hangjin <pusanmice@hanmail.net>
2020-02-15 20:40:45 +0900

    111

---
+++ src/main/java/main/Application.java
@@ -0,0 +1,20 @@
+package main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan("main/bookstore")
+@EnableAutoConfiguration
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(Application.class);
+        System.out.println("AAAAAA");
+        System.out.println("AAAAAA");
+        application.run(args);
+    }
+}
+
